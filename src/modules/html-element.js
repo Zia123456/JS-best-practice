dd6319166533c8fb2ae73388a4f1281b18955508
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-11-11 12:50:23 -0800

    project initialized

---
+++ src/modules/html-element.js
@@ -0,0 +1,10 @@
+/* eslint-disable object-curly-newline */
+const listDiv = document.querySelector('.to-do-list-container');
+const input = document.querySelector('.input');
+const taskDiv = document.querySelector('.to-do');
+const dots = document.querySelector('dots-icon');
+const remove = document.querySelector('.remove');
+const enter = document.querySelector('.enter-icon');
+const deletecompletedTasks = document.querySelector('.clear-all-completed');
+
+export { input, listDiv, dots, taskDiv, remove, enter, deletecompletedTasks };
